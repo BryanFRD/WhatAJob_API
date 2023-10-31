44489ed31e31d0413aa94d37a3cbcea3d80b312b
Bryan <bryan.ferrando@epitech.eu>
2023-10-31 15:59:36 +0000

    Initialized What A Job back-end

---
+++ src/main/java/fr/whatajob/WhatAJob/WhatAJobApplication.java
@@ -0,0 +1,13 @@
+package fr.whatajob.WhatAJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatAJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatAJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/whatajob/WhatAJob/WhatAJobApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.whatajob.WhatAJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatAJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
